492cfb73a0f2e03e2ea87078e461fffe134bf9df
GBaykov <73140481+GBaykov@users.noreply.github.com>
2021-02-11 19:30:47 +0300

    fix: add code

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function reverse (n) {
-  
+
+ let result = "";
+   let str = String(Math.abs(n));
+ for (i= str.length-1; i >=0; i=i-1){
+ result =  result +str[i] ;
+
+ }
+ return result;
 }
